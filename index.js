9bf0b18b269dab7b896e60312d54d19715e16c1a
Ahmad Shah <ah.seerat@gmail.com>
2022-06-26 23:43:41 -0400

    commit tree 50e84cefec3b2f163bed2b851e5ea54910db8c26

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+
+
+// const append = ["Milo",
+// "Otis",
+// "Garfield",
+// "Odie"]
+
+// function arr(append) {
+//     append.push("Odie")
+//     append.pop()
+//   }
+
+
+
+
+
+// const function append() {
+
+// const append = [
+//       "Milo",
+//       "Otis",
+//       "Garfield",
+//        "Odie"
+//      ]
+
+
+const append = ["Milo", "Otis", "Garfield"]
+append.push("Odie")
+
+const prepend = ["Milo", "Otis", "Garfield"]
+prepend.unshift("Odie")
+
+const removeLast = ["Milo", "Otis", "Garfield"]
+removeLast.pop("Garfield")
+
+const removeFirst = ["Milo", "Otis", "Garfield"]
+removeFirst.shift("Milo")
+
